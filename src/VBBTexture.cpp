901c3bf705bdac3a3ab9da888d28a12a7965604a
richard-starstone <30851834+StarstoneSoftware@users.noreply.github.com>
2023-12-17 09:10:46 -0500

    Texture fix

--- src/VBBTexture.cpp
+++ src/VBBTexture.cpp
@@ -193,7 +193,7 @@ bool VBBTexture::loadRawTexture(VBBBufferDynamic &imageBuffer, VkFormat format, 
     imageInfo.format = imageFormat;
     imageInfo.tiling = VK_IMAGE_TILING_OPTIMAL;
     imageInfo.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
-    imageInfo.usage = VK_IMAGE_USAGE_SAMPLED_BIT;// -> unless you want to also use it as image storage... VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_STORAGE_BIT;
+    imageInfo.usage = VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_STORAGE_BIT;
     imageInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
     imageInfo.samples = VK_SAMPLE_COUNT_1_BIT;
     imageInfo.flags = 0;
